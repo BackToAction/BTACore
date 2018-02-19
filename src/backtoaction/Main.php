0d995f324d520d341d41498849d4942b9c5a610a
HyGlobalHD <GamerXzavier@gmail.com>
2018-02-19 14:26:50 +0800

    Main still not finished.

--- src/backtoaction/Main.php
+++ src/backtoaction/Main.php
@@ -1,1 +1,32 @@
+<?php
+/**
+ * Copyrights 2018 BackToAction's Team
+ * Created By @HyGlobalHD At 5:27 PM 16/02/2018
+ */
+namespace backtoaction;
 
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
+
+    const API_VERSION = 0.01; // This plugin API_VERSION
+
+    public function onEnable() {
+        // code...
+        $this->loadAll();
+    }
+
+    public function loadAll() {
+        // code...
+    }
+
+    public function saveAll() {
+        // code...
+    }
+
+    public function onDisable() {
+        // code...
+        $this->saveAll();
+    }
+
+
+}
+?>
